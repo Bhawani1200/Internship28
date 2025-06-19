d11b101fc667e9d013c14f63b7fd1cbbfd96079a
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-06-19 16:33:00 +0545

    order API

--- Intern/Backend/src/main/java/com/chaubisedhaka/Backend/service/OrderService.java
+++ Intern/Backend/src/main/java/com/chaubisedhaka/Backend/service/OrderService.java
@@ -1,11 +1,29 @@
 package com.chaubisedhaka.Backend.service;
 
+import com.chaubisedhaka.Backend.exception.OrderException;
 import com.chaubisedhaka.Backend.model.Address;
 import com.chaubisedhaka.Backend.model.User;
-import org.hibernate.query.Order;
+import com.chaubisedhaka.Backend.model.Order;
+import java.util.List;
 
 public interface OrderService {
 
     public Order createOrder(User user, Address shippingAddress);
+
+    public Order findOrderById(Long orderId) throws OrderException;
+
+    public List<Order>userOrderHistory(Long userId);
+
+    public Order confirmOrder(Long orderId) throws OrderException;
+
+    public Order shippedOrder(Long orderId) throws OrderException;
+
+    public Order deliveredOrder(Long orderId) throws OrderException;
+
+    public Order canceledOrder(Long orderId) throws OrderException;
+
+    public List<Order>getALlOrders();
+
+    public void deleteOrder(Long orderId)throws OrderException;
 
 }
