8de9b2e8b69c9904a6a99861040f8b7f4d9282d8
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-06-20 15:30:36 +0545

    for cart implementation

--- Intern/Backend/src/main/java/com/chaubisedhaka/Backend/repository/CartRepository.java
+++ Intern/Backend/src/main/java/com/chaubisedhaka/Backend/repository/CartRepository.java
@@ -2,7 +2,12 @@ package com.chaubisedhaka.Backend.repository;
 
 import com.chaubisedhaka.Backend.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CartRepository extends JpaRepository<Cart,Long> {
+
+    @Query("SELECT c From Cart c Where c.user.id=:userId")
+    public Cart findByUserId(@Param("userId")Long userId);
 
 }
