dc77fcb08ac620a718e34f7eead4a1ec7aad0416
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-06-21 21:11:28 +0545

    product controller

---
+++ Intern/Backend/src/main/java/com/chaubisedhaka/Backend/controller/ProductController.java
@@ -0,0 +1,34 @@
+package com.chaubisedhaka.Backend.controller;
+
+import com.chaubisedhaka.Backend.exception.ProductException;
+import com.chaubisedhaka.Backend.model.Product;
+import com.chaubisedhaka.Backend.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    private ProductService productService;
+
+    @GetMapping("/products")
+    public ResponseEntity<Page<Product>>findProductByCategoryHandler(@RequestParam String category, @RequestParam List<String>color, @RequestParam List<String>size,
+                                                                                      @RequestParam Integer minPrice, @RequestParam Integer maxPrice, @RequestParam String sort,
+                                                                                      @RequestParam String stock, @RequestParam Integer pageNumber, @RequestParam Integer minDiscount,
+                                                                                      @RequestParam Integer pageSize){
+        Page<Product>res=productService.getAllProducts( category, color, size, minPrice, maxPrice, minDiscount, sort, stock, pageNumber, pageSize);
+        System.out.println("complete products");
+        return  new ResponseEntity<>(res, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/products/id/{productId}")
+    public ResponseEntity<Product>findProductByIdHandler(@PathVariable Long productId)throws ProductException{
+        Product product=productService.findProductById(productId);
+        return new ResponseEntity<Product>(product,HttpStatus.ACCEPTED);
+    }
+
+}
