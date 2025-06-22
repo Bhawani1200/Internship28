eb14f9f53259a6b2b50920da55acfcc5cda713f2
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-06-22 18:31:24 +0545

    controllers

--- Intern/Backend/src/main/java/com/chaubisedhaka/Backend/controller/ReviewController.java
+++ Intern/Backend/src/main/java/com/chaubisedhaka/Backend/controller/ReviewController.java
@@ -2,12 +2,12 @@ package com.chaubisedhaka.Backend.controller;
 
 import com.chaubisedhaka.Backend.exception.ProductException;
 import com.chaubisedhaka.Backend.exception.UserException;
-import com.chaubisedhaka.Backend.model.Address;
-import com.chaubisedhaka.Backend.model.Order;
 import com.chaubisedhaka.Backend.model.Rating;
+import com.chaubisedhaka.Backend.model.Review;
 import com.chaubisedhaka.Backend.model.User;
 import com.chaubisedhaka.Backend.request.RatingRequest;
-import com.chaubisedhaka.Backend.service.RatingService;
+import com.chaubisedhaka.Backend.request.ReviewRequest;
+import com.chaubisedhaka.Backend.service.ReviewService;
 import com.chaubisedhaka.Backend.service.UserService;
 import jdk.jshell.spi.ExecutionControl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,31 +18,32 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/ratings")
-public class RatingController {
+@RequestMapping("/api/reviews")
+public class ReviewController {
+
     @Autowired
-    private RatingService ratingService;
+    private ReviewService reviewService;
 
     @Autowired
     private UserService userService;
 
+
     @PostMapping("/create")
-    public ResponseEntity<Rating> createRating(@RequestBody RatingRequest req, @RequestHeader("Authorization")String jwt) throws UserException, ProductException, ExecutionControl.UserException {
+    public ResponseEntity<Review>createReview(@RequestBody ReviewRequest req, @RequestHeader("Authorization")String jwt) throws UserException, ProductException, ExecutionControl.UserException {
         User user=userService.findUserProfileByJwt(jwt);
 
-        Rating rating=ratingService.createRating(req,user);
+        Review review=reviewService.createReview(req,user);
 
-        return new ResponseEntity<Rating>(rating, HttpStatus.CREATED);
+        return new ResponseEntity<>(review, HttpStatus.CREATED);
     }
 
     @GetMapping("/product/{productId}")
-    public ResponseEntity<List<Rating>> getProductsRating(@PathVariable Long productId, @RequestHeader("Authorization")String jwt) throws UserException, ProductException, ExecutionControl.UserException {
+    public ResponseEntity<List<Review>> getProductsRating(@PathVariable Long productId, @RequestHeader("Authorization")String jwt) throws UserException, ProductException, ExecutionControl.UserException {
         User user=userService.findUserProfileByJwt(jwt);
 
-        List<Rating> ratings=ratingService.getProductsRating(productId);
+        List<Review> reviews=reviewService.getAllReview(productId);
 
-        return new ResponseEntity<>(ratings, HttpStatus.CREATED);
+        return new ResponseEntity<>(reviews, HttpStatus.CREATED);
     }
-
 
 }
---
+++ Intern/Backend/src/main/java/com/chaubisedhaka/Backend/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.chaubisedhaka.Backend.controller;
+
+import com.chaubisedhaka.Backend.exception.UserException;
+import com.chaubisedhaka.Backend.model.User;
+import com.chaubisedhaka.Backend.service.UserService;
+import jdk.jshell.spi.ExecutionControl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/profile")
+    public ResponseEntity<User>getUserProfileHandler(@RequestHeader("Authorization")String jwt) throws UserException, ExecutionControl.UserException {
+        User user=userService.findUserProfileByJwt(jwt);
+
+        return new ResponseEntity<User>(user, HttpStatus.ACCEPTED);
+    }
+}
