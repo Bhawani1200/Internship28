0e9bd256289c0ff9d605175157325954fb5cd5d0
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-07-04 16:01:01 +0545

    admin dashboard

--- Intern/Frontend/src/Admin/components/Admin.jsx
+++ Intern/Frontend/src/Admin/components/Admin.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { useTheme } from "styled-components";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useNavigate } from "react-router-dom";
+import Box from "@mui/material/Box";
+
+const menu = [
+  { name: "Dashboard", path: "/admin" },
+  { name: "Products", path: "/admin/products" },
+  { name: "Customers", path: "/admin/users" },
+  { name: "Orders", path: "/admin/orders" },
+  { name: "AddProduct", path: "/admin/product/create" },
+];
 
 const Admin = () => {
+  const theme = useTheme();
+  const isLargeScreen = useMediaQuery(theme.breakpoint.up("lg"));
+  const [sideBarVisible, setSideBarVisible] = useState(false);
+  const navigate = useNavigate();
+
+  const drawer = <Box></Box>;
+
   return <div>I am the admin.</div>;
 };
 
